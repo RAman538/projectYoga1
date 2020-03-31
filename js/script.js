340e96d5b5fae1da00761894aadf9466078d0a9d
RAman <vv219901@gmail.com>
2020-03-31 23:10:51 +0700

    create timer

--- js/script.js
+++ js/script.js
@@ -35,6 +35,50 @@ let info = document.querySelector('.info-header'),
         }
     });
 
+    // Timers:
+
+    let deadline = '2020-06-02';
+
+    function getTimeRemaining(endtime) {
+         let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/1000/60/60);
+
+            return {
+                'total': t,
+                'seconds': seconds,
+                'minutes': minutes,
+                'hours': hours,
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            hours.textContent = (t.hours);
+            minutes.textContent = ('0' + t.minutes).slice(-2);
+            seconds.textContent = ('0' + t.seconds).slice(-2);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
 
 });
 
